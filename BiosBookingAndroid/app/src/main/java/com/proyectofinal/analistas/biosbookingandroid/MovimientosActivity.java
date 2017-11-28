7f2e6cce3b8c487ade10b7ab4a7e97616044e59c
Geronimo <geronimoamarillo29@gmail.com>
2017-11-28 20:44:32 -0300

    Repositorio Inicial

---
+++ BiosBookingAndroid/app/src/main/java/com/proyectofinal/analistas/biosbookingandroid/MovimientosActivity.java
@@ -0,0 +1,13 @@
+package com.proyectofinal.analistas.biosbookingandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MovimientosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movimientos);
+    }
+}
---
+++ BiosBookingAndroid/app/src/main/java/com/proyectofinal/analistas/biosbookingandroid/ObraDetailActivity.java
@@ -0,0 +1,13 @@
+package com.proyectofinal.analistas.biosbookingandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ObraDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_obra_detail);
+    }
+}
