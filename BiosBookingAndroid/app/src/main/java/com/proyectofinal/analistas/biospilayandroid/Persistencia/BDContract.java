52b4e1ccc34f046d6a0b9c1b34328f2d5fab63e6
Geronimo <geronimoamarillo29@gmail.com>
2017-12-15 23:10:35 -0300

    Anclaje antes de arrancar Listados

--- BiosBookingAndroid/app/src/main/java/com/proyectofinal/analistas/biospilayandroid/Persistencia/BDContract.java
+++ BiosBookingAndroid/app/src/main/java/com/proyectofinal/analistas/biospilayandroid/Persistencia/BDContract.java
@@ -29,21 +29,21 @@ public class BDContract {
         public static final String COLUMNA_DIRECCION = "Direccion";
         public static final String COLUMNA_FOTO = "Foto";
 
-        public static final String[] COLUMNAS = { _ID, COLUMNA_FECHA_CONTRATO, COLUMNA_METROS_CUADRADOS};/*, COLUMNA_NOMBRE_CLIENTE, COLUMNA_DIRECCION, COLUMNA_FOTO };*/
+        public static final String[] COLUMNAS = { _ID, COLUMNA_FECHA_CONTRATO, COLUMNA_METROS_CUADRADOS, COLUMNA_NOMBRE_CLIENTE, COLUMNA_DIRECCION, COLUMNA_FOTO };
 
         public static final String SQL_CREAR_TABLA = new StringBuilder("CREATE TABLE ").append(TABLA_OBRA).append(" (")
                 .append(_ID).append(" INTEGER PRIMARY KEY AUTOINCREMENT, ")
-                .append(COLUMNA_FECHA_CONTRATO).append(" DATETIME NOT NULL, ")
-                .append(COLUMNA_METROS_CUADRADOS).append(" DECIMAL NOT NULL, ")
-                .append(COLUMNA_NOMBRE_CLIENTE).append(" STRING NOT NULL, ")
-                .append(COLUMNA_DIRECCION).append(" STRING NOT NULL, ")
-                .append(COLUMNA_FOTO).append(" STRING NOT NULL);").toString();
+                .append(COLUMNA_FECHA_CONTRATO).append(" TEXT NOT NULL, ")
+                .append(COLUMNA_METROS_CUADRADOS).append(" REAL NOT NULL, ")
+                .append(COLUMNA_NOMBRE_CLIENTE).append(" TEXT NOT NULL, ")
+                .append(COLUMNA_DIRECCION).append(" TEXT NOT NULL, ")
+                .append(COLUMNA_FOTO).append(" TEXT NOT NULL);").toString();
 
         public static final String SQL_ELIMINAR_TABLA = new StringBuilder("DROP TABLE IF EXISTS ").append(TABLA_OBRA).append(";").toString();
 
-        public static final String SQL_INSERTAR_DATOS_INICIALES = new StringBuilder("INSERT INTO ").append(TABLA_OBRA).append(" VALUES (20161012, 1000, Pepe Chulo, Avenida Siempre Viva 1234, .../ruta.jpg);")
-                .append("INSERT INTO ").append(TABLA_OBRA).append(" VALUES (20170925, 750, Roberto Gomez, 21 Jump Street, .../ruta.jpg);")
-                .append("INSERT INTO ").append(TABLA_OBRA).append(" VALUES (20170521, 800, Mary Curie, LaCalle Pou, .../ruta.jpg);").toString();
+        public static final String SQL_INSERTAR_DATOS_INICIALES = new StringBuilder("INSERT INTO ").append(TABLA_OBRA).append(" VALUES (2017-11-21 14: 35: 00.000, 1000, Pepe Chulo, Avenida Siempre Viva 1234, .../ruta.jpg);")
+                .append("INSERT INTO ").append(TABLA_OBRA).append(" VALUES (2017-11-21 14: 35: 00.000, 750, Roberto Gomez, 21 Jump Street, .../ruta.jpg);")
+                .append("INSERT INTO ").append(TABLA_OBRA).append(" VALUES (2017-11-21 14: 35: 00.000, 800, Mary Curie, LaCalle Pou, .../ruta.jpg);").toString();
 
     }
 
@@ -60,7 +60,7 @@ public class BDContract {
         public static final String SQL_CREAR_TABLA = new StringBuilder("CREATE TABLE ").append(TABLA_MATERIAL).append(" (")
                 .append(COLUMNA_STOCK).append(" INTEGER NOT NULL, ")
                 .append(COLUMNA_NOMBRE).append(" TEXT NOT NULL, ")
-                .append(COLUMNA_FECHA_ALTA).append(" DATETIME NOT NULL), ")
+                .append(COLUMNA_FECHA_ALTA).append(" TEXT NOT NULL), ")
                 .append(COLUMNA_DESCRIPCION).append(" TEXT NOT NULL, ")
                 .append(COLUMNA_OBRA).append(" INTEGER NOT NULL FOREIGN KEY REFERENCES " + TABLA_OBRA + "("+ Obras._ID +")")
                 .append("PRIMARY KEY ("+ COLUMNA_NOMBRE +", "+ COLUMNA_OBRA +");").toString();
@@ -85,7 +85,7 @@ public class BDContract {
                 .append(COLUMNA_OBSERVACION).append(" TEXT NOT NULL, ")
                 .append(COLUMNA_MATERIAL).append(" INTEGER NOT NULL FOREIGN KEY REFERENCES " + TABLA_MATERIAL + " ( " + Materiales.COLUMNA_NOMBRE + "), ")
                 .append(COLUMNA_OBRA).append(" INTEGER NOT NULL FOREIGN KEY REFERENCES " + TABLA_MATERIAL + "( " + Materiales.COLUMNA_OBRA + "),")
-                .append(COLUMNA_FECHA_MOVIMIENTO).append(" DATETIME NOT NULL, ")
+                .append(COLUMNA_FECHA_MOVIMIENTO).append(" TEXT NOT NULL, ")
                 .append("PRIMARY KEY ("+ COLUMNA_OBRA +", " + COLUMNA_MATERIAL +", " + _ID + " );").toString();
 
 
